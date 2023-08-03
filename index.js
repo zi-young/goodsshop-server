d3dd11ae6c45c127e5a79ab101b5f0e27c30d6c1
jiyoung <jypark912@naver.com>
2023-08-03 11:56:35 +0900

    server설정

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-var http=require('http');
-var hostname='127.0.0.1';
-var port="8080";
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const port="8080";
 
-const server=http.createServer(function(req, res){
-    const path = req.url;
-    const method = req.method;
-    if(path==='/products'){
-        if(method === "GET"){
-            res.writeHead(200, {"Content-Type": "application/json"});
-            const products=JSON.stringify([
-                {
-                    name: "독버섯 조명",
-                    price: 89000
-                }   
-            ]);
-            res.end(products);
-        }else if(method === "POST"){
-            res.end("생성되었습니다.")
-        }
-    }
-    res.end("Good Bye");
+
+
+app.use(express.json());
+app.use(cors()) //브라우저의 cors 이슈를 막기 위해 사용하는 코드
+app.get('/products', (req, res) => {
+    const queryString=req.query;
+    console.log('queryString:', query)
+    res.send({
+        products: [
+            {id:0, name : "독버섯조명", price:89000, seller : "유니스", imageUrl:"shop-test/img/products/product01.jpg"},
+            {id:1, name : "허리아작의자", price:91000, seller : "더휴먼", imageUrl:"shop-test/img/products/product02.jpg"},
+            {id:2, name : "잘록거울", price:180000, seller : "더부엌", imageUrl:"shop-test/img/products/product03.jpg"}
+        ]
+    })
 })
 
-server.listen(port, hostname);
-console.log('server on')
+app.post('/products', (req, res) => {
+    const body=req.body;
+    res.send({
+        body,
+    })
+})
+
+app.get("/products/:id", (req, res)=>{
+    const params=req.params;
+    const {id} = params;
+    res.send(`id는 ${id} 입니다`);
+})
+app.listen(port, ()=>{
+    console.log('server on')
+});
